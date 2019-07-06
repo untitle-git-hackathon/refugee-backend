d0b8601b19d0523041d712c444a332c645c072b1
Hoseong Son <lsookcha@me.com>
2019-07-06 21:22:34 +0900

    Add is_from_refugee and image_url field to story

--- refugee/models/location.py
+++ refugee/models/location.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class Location(models.Model):
     name = models.CharField(max_length=30)
+    description = models.TextField()
+    url = models.TextField()
--- refugee/models/story.py
+++ refugee/models/story.py
@@ -4,3 +4,5 @@ from .refugee import Refugee
 class Story(models.Model):
     message = models.TextField()
     refugee_id = models.OneToOneField(to=Refugee, on_delete=models.CASCADE)
+    is_from_refugee = models.BooleanField()
+    image_url = models.TextField()
